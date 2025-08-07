24e618593a5b09dc077672d3abef3872d652eafd
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2025-08-07 13:19:53 +0300

    Added JS File

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// add active class Dynamically
+// in github it seems that href have repo link and github page link so will take the last part where path across
+
+let websiteUrl = window.location.pathname;
+console.log(websiteUrl);
+let navLink = document.querySelectorAll("nav ul li a");
+
+console.log(navLink[0].pathname);
+navLink.forEach(function (el) {
+    if (el.pathname === websiteUrl) {
+        el.classList.add("active");
+    } else if (el.pathname === "/") {
+        el.classList.add("active");
+    } // handle the case if the last of pathname is / void
+});
